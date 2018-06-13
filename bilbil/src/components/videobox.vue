9b02601c220a52f20dc9cdb8013e83a6db159817
magicyangmei <magicyangmei@163.com>
2018-06-13 20:20:53 +0800

    修改videobox

---
+++ bilbil/src/components/videobox.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div v-for="(item,i) in initData" :key="i">
+            <p>{{item.title}}</p>
+
+
+
+            <p>1111111111111111111</p>
+
+
+
+
+
+            <img :src="item.pic" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+import * as hostUrl  from '../service/Bmain'
+import axios from 'axios';
+//axios  
+    export default {
+        name:'videobox',
+        created(){
+
+        },
+        data(){
+            return{
+                initData:''
+            }
+        },
+        mounted(){
+            //hostUrl.MainUrl()
+            axios.get(hostUrl.MainUrl())
+                .then(res=>res)
+                .then(response=>{
+                    console.log(response)
+                    this.initData = response.data.data.list
+                })
+            
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
